bc20f885d83157c4ef9644cb0f5e676292fa1f1e
Zac Coeur <coeurzac@gmail.com>
2022-12-21 23:09:49 -0500

    about done

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,6 +18,7 @@ data Knowledge = Knowledge { green :: Set.Set (Int, Char)
 initKnowledge :: Knowledge
 initKnowledge = Knowledge Set.empty Set.empty Set.empty
 
+--play game in parallel
 playPar :: B.ByteString -> B.ByteString -> Knowledge -> Set.Set B.ByteString -> Int -> IO ()
 playPar g a k gs gCount = do
    if g == a then do
@@ -29,7 +30,7 @@ playPar g a k gs gCount = do
       print g'
       playPar g' a k' gs' (gCount+1)
 
-
+-- play game sequentially
 playSeq :: B.ByteString -> B.ByteString -> Knowledge -> Set.Set B.ByteString -> Int -> IO ()
 playSeq g a k gs gCount = do
    if g == a then do
@@ -77,40 +78,48 @@ Takes a guess and returns a Set of valid possible words.
 possibleWords:: Lib.Knowledge -> Set.Set B.ByteString -> Set.Set B.ByteString
 possibleWords k g = Set.intersection (Set.intersection (matchGreens (Set.toList (green k)) g) (matchYellows (Set.toList (yellow k)) g)) (matchGreys (Set.toList (grey k)) g)
 
+-- Computes the yellow letters of a guess for a given answer 
 getYellows :: B.ByteString -> B.ByteString -> Set.Set (Int, Char)
 getYellows g a = l `Set.difference` getGreens g a
    where s = Set.fromList $ B.unpack a
          l = Set.fromList $ filter (\x -> snd x `Set.member` s) ([0..] `zip` B.unpack g)
 
+-- Computes the green letters of a guess for a given answer 
 getGreens:: B.ByteString -> B.ByteString -> Set.Set (Int, Char)
 getGreens g a = Set.fromList ([0..] `zip` B.unpack g) `Set.intersection` Set.fromList ([0..] `zip` B.unpack a)
 
+-- Computes the grey letters of a guess for a given answer 
 getGreys:: B.ByteString -> B.ByteString -> Set.Set Char
 getGreys g a = s `Set.difference` ng
    where s = Set.fromList $ B.unpack g
          ng = Set.map snd (getGreens g a) `Set.union` Set.map snd (getYellows g a)
 
+-- Updates knowledge for a guess-answer pair
 addToKnowledge :: Knowledge -> B.ByteString -> B.ByteString -> Knowledge
 addToKnowledge k g a = Knowledge greens yellows greys
    where greens = green k `Set.union` getGreens g a
          yellows = yellow k `Set.union` getYellows g a
          greys = grey k `Set.union` getGreys g a
 
+-- Creates a frequency map from a list
 count :: Ord a => [a] -> Map.Map a Float
 count = Map.fromListWith (+) . (`zip` repeat 1)
 
+-- computes the entropy of a guess
 entropy :: B.ByteString -> Knowledge -> Set.Set B.ByteString ->Float
 entropy g k as = Map.foldl (+) 0 $ Map.unionWith (*) p inf
    where l = fromIntegral (Set.size as)
          p =  Map.map (/l) $ count (map (addToKnowledge k g) (Set.toList as))
          inf = Map.map (\x ->logBase 2 (1/x)) p
 
+-- parallel computes the entropies of all guesses
 entropiesPar :: Set.Set B.ByteString -> Knowledge -> [(B.ByteString, Float)]
 entropiesPar gs k = e_list
    where g_list = Set.toList gs
          as = possibleWords k gs
          e_list = map (\g -> (g, entropy g k as)) g_list `using` parBuffer 200 rdeepseq
 
+-- sequentially computes the entropies of all guesses
 entropies :: Set.Set B.ByteString -> Knowledge -> [(B.ByteString, Float)]
 entropies gs k = e_list
    where g_list = Set.toList gs
@@ -123,12 +132,14 @@ maxEntropyHelper (max_guess, max_entr) ((guess, entr):xs)
    | entr > max_entr = maxEntropyHelper (guess, entr) xs
    | otherwise = maxEntropyHelper (max_guess, max_entr) xs
 
+-- parallel computes the maximum entropy of all guesses
 maxEntropyPar :: Set.Set B.ByteString -> Knowledge ->  (B.ByteString, Float)
 maxEntropyPar gs k
    | Set.size as == 1 = (Set.elemAt 0 as, 0)
    | otherwise = maxEntropyHelper (B.empty, -1) (entropiesPar gs k)
    where as = possibleWords k gs
 
+-- sequentially computes the maximum entropy of all guesses
 maxEntropy :: Set.Set B.ByteString -> Knowledge ->  (B.ByteString, Float)
 maxEntropy gs k
    | Set.size as == 1 = (Set.elemAt 0 as, 0)
